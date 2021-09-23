f8cc6619beb0cdbd81335c0bbc461963ebed0c6d
mgriffith87 <88860390+mgriffith87@users.noreply.github.com>
2021-09-23 16:12:18 -0700

    fix script/ sheet

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
-function showTemperature(response) {
-  response.preventDefault();
-  let temperature = Math.round(response.data.main.temp);
+let apiKey = "6ed4b8240db798c9a2cc708c63ab3c0c&units=metric";
 
-  let updateP = document.querySelector("#show-temperature");
-  updateP.innerHTML = "It is ${temperature}°F in ${city.value}";
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  (cityElement.innerHTML = response), data.name;
+  (descriptionElement.innerHTML = response), data, weather[0].description;
+  humiditiyElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = formatDate(response.data.dt * 1000);
 }
+
+function displayFahrenheitTemperature(event) {
+ event.preventDefault();
+ let temperatureElement =document.querySelector("#temperature");
+
+celsiusLink.classList.remove("active");
+fahrenheitLink.classList.add("active");
+let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+temperatureElement.innerHTML = Math.round(fahreneitTemperaure);
+}
+
+let celsiusTemperaure = null;
+
+let fahrenheitLink =document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperaure);
+
+let celsiusLink =document.querySelector("celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusLink)
+
+search("Pollock Pines");
+
+
+let iconElement = document.querySelector("#icon");
+iconElement.setAttribute("src", 'http://openweathermap.org/img/wn/${response.data.weather[0].icon}'
+
+
+
+
+
+axios.get(apiUrl).then(showTemperature);
 
 function editLocation(event) {
   event.preventDefault();
@@ -12,21 +51,21 @@ function editLocation(event) {
 
   let city = document.querySelector("#selector-city").value;
   h1.innerHTML = city;
-  let apiKey = "6ed4b8240db798c9a2cc708c63ab3c0c";
   let apiUrl =
     "https://api.openweather.org/data/2.5/weather?q=${city}&appid=6ed4b8240db798c9a2cc708c63ab3c0c&units=metric";
 
   axios.get(apiUrl).then(showTemperature);
 }
 
-function showCurrentPosition(event) {
+function search(event) {
   event.preventDefault();
+let cityInputElement =document.querySelector("#city-input");
+console.log(cityinputElement.value);
+}
 
   navigator.geolocation.getCurrentPosition(currentPosition);
 }
 
-let formSearch = document.querySelector("form");
-formSearch = document.addEventListener("submit", editLocation);
 
 console.log(days);
 let now = new Date();
@@ -45,26 +84,7 @@ let days = [
   "saturday",
 ];
 let day = days[now.getDay()];
-let months = [
-  "january",
-  "february",
-  "march",
-  "april",
-  "may",
-  "june",
-  "july",
-  "august",
-  "september",
-  "october",
-  "november",
-  "december",
-];
 
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", search);
 
-function search(event) {
-  event.preventDefault();
-  let cityInputElement = document.querySelector("#city-input");
-  console.log(cityInputElement.value);
-}
+let form =document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
